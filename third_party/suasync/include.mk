1d21c7703705ad163a3da6f2a17edbec308286ae
laudukang <laudukang@gmail.com>
2019-11-18 16:45:38 +0800

    update async to 1.4.1
    Signed-off-by: laudukang <laudukang@gmail.com>

--- third_party/suasync/include.mk
+++ third_party/suasync/include.mk
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
-SUASYNC_VERSION := 1.4.0
+SUASYNC_VERSION := 1.4.1
 SUASYNC := third_party/suasync/async-$(SUASYNC_VERSION).jar
 SUASYNC_BASE_URL := http://central.maven.org/maven2/com/stumbleupon/async/$(SUASYNC_VERSION)
 
